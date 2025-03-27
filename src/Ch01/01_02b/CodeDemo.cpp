3c91ec5f89a7eddcb48dfd2b2bc7a0ac2fb8a69f
FiiFaCZ <fprdesign@gmail.com>
2025-03-27 08:38:19 +0000

    HW Finished

--- src/Ch01/01_02b/CodeDemo.cpp
+++ src/Ch01/01_02b/CodeDemo.cpp
@@ -5,5 +5,9 @@
 #include <iostream>
 
 int main(){
-    std::cout << "Hello World!!";
+    std::cout << "Hello World!!" << std::endl;
+
+    std::cout << std::endl << std::endl;
+
+    return (0);
 }
